38cec3da94f9a85d5d5de155b601607c75d3a448
phili <phili@192.168.0.12>
2019-09-13 14:31:41 -0400

    Filled in driver file code

--- CHG4343_Fall_2018_Absorption_Column/src/CHG4343_Fall_2018_Absorption_Column.java
+++ CHG4343_Fall_2018_Absorption_Column/src/CHG4343_Fall_2018_Absorption_Column.java
@@ -1,7 +1,12 @@
-
 public class CHG4343_Fall_2018_Absorption_Column {
-	public static void main (String[] args) {
-		System.out.println("Hello World!");
-	}
-
-}
+  public static void main(String[] args)
+  {
+    //Menu.clear();
+    System.out.println("|=============================================================|");
+    System.out.println("|                  Welcome to AbsorbSim Pro                   |");
+    System.out.println("|=============================================================|");
+    //Menu.mainMenu();
+  } // End of main method
+} // End of AbsorptionDriver class
+ 
+   
